f034569e2c55881124bb8f23bded92a8a6252526
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-07 11:10:08 +0100

    operacje warunkowe

--- lab_2/operacje_warunkowe.py
+++ lab_2/operacje_warunkowe.py
@@ -1,34 +1,54 @@
-x = int(input("Podaj x: "))
-y = 5*(x == 10)
+# x = int(input("Podaj x: "))
+# y = 5*(x == 10)
 
-if x > 5:
-    print(f'Wynik: {x}')
-else:
-    print("End")
+# if x > 5:
+#     print(f'Wynik: {x}')
+# else:
+#     print("End")
 
 # print(f'{y}')
 
-if x == 1:
-    print("Jeden")
-elif x == 2:
-    print("Dwa")
-else:
-    print("Więcej")
+# if x == 1:
+#     print("Jeden")
+# elif x == 2:
+#     print("Dwa")
+# else:
+#     print("Więcej")
+#
+# if not x > 5 and y < 3:
+#     print("OK")
+#
+# l = {1, 2, 3}
 
-if not x > 5 and y < 3:
-    print("OK")
-
-l = {1, 2, 3}
-
-if x in l:
-    print("Yes")
-else:
-    print("No")
+# if x in l:
+#     print("Yes")
+# else:
+#     print("No")
 
 
-v = {True, True, False}
-any(v)
+# v = {True, True, False}
+# any(v)
+#
+# z = int(input())
+# x = z if z > 5 else 10*z
+# print(f'Z: {x}')
 
-z = int(input())
-x = z if z > 5 else 10*z
-print(f'Z: {x}')
+s = "Ala ma kota"
+
+# for i in s:
+#     print(i, end="")
+
+# for x in [1, 2, 3, 4, 5]:
+#     print(f'{x**2}')
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+for i in range(20):
+    if i % 2 == 0:
+        continue
+
+    print(i, end=" ")
+
+dict = {"a": 10, "b": 20, "c": 25}
+print(dict.get("b"))
---
+++ lab_2/zadanie.py
@@ -0,0 +1,10 @@
+# start_point = "██ █    █  █   █     █      ██"
+
+#Choinka
+
+n = int(input("Podaj n: "))
+
+for i in range(1, n+1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+    if i == n:
+        print(" " * (n-3), "||")
