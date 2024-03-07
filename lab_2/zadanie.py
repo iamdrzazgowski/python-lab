133fd08aa2125d4fd2c121079e5c1c7eac6c1d0c
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-07 11:17:45 +0100

    wyświetlanie choinki

--- lab_2/zadanie.py
+++ lab_2/zadanie.py
@@ -4,7 +4,11 @@
 
 n = int(input("Podaj n: "))
 
-for i in range(1, n+1):
-    print(" " * (n - i) + "#" * (2 * i - 1))
-    if i == n:
-        print(" " * (n-3), "||")
+# for i in range(1, n+1):
+#     print(" " * (n - i) + "#" * (2 * i - 1))
+#     if i == n:
+#         print(" " * (n-3), "||")
+
+for i in range(n + 1):
+    print(" " * (n-i) + "*" * (i == 0) + "#" * (2*i+1) * (i > 0))
+print(" " * (i-1) + "| |")
