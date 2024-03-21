b22251682d3e3c7f33b2e5fa28fa141c85f14fc5
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-21 11:42:03 +0100

    lab_4 ćwiczenie list składanych oraz metod map i filter

--- lab_4/zadania_cw.py
+++ lab_4/zadania_cw.py
@@ -10,6 +10,7 @@ print(list(map(lambda x: x * 2 if x % 2 == 0 else x,list(filter(lambda x: x % 3 
 print(list([i * 2 if i % 2 == 0 else i for i in list([j for j in lista if j % 3 != 0])]))
 
 """Zadanie 9"""
+
 print(f'Zadanie 9')
 
 slowa = ["ala", "ma69", "3254"]
@@ -21,6 +22,7 @@ print(list(map(lambda x: ''.join(filter(str.isalpha, x)), slowa)))
 print(list(''.join([i for i in j if i.isalpha()]) for j in slowa))
 
 """  Zadanie 10  """
+
 print(f'Zadanie 10')
 # Użycie map i filter
 print(list(map(int, filter(bool, map(lambda j: ''.join(filter(lambda i: i.isdigit(), j)), slowa)))))
@@ -40,27 +42,32 @@ print(list(map(lambda krotka: tuple(krotka[:2]), filter(lambda krotka: len(krotk
 print(list([tuple(krotka[:2]) for krotka in lista_krotek if len(krotka) >= 2]))
 
 """Zadanie 12"""
+
 print(f'Zadanie 12')
 
 print(list(filter(lambda x: sum(x) % 2 == 0, lista_krotek)))
 
 """Zadanie 13"""
+
 print(f'Zadanie 13')
 
 print(list([krotka for krotka in lista_krotek if all(element % 3 == 0 for element in krotka)]))
 
 """Zadanie 14"""
+
 print(f'Zadanie 14')
 
 print(list([krotka for krotka in lista_krotek if any(element % 3 == 0 for element in krotka)]))
 
 """Zadanie 15"""
+
 print(f'Zadanie 15')
 
 lista_slow = ["Ala ma kota", "pies", "Jakub", "Opole Lubelskkie"]
 print(list([element.split()[0] for element in lista_slow if ' ' in element]))
 
 """Zadanie 16"""
+
 print(f'Zadanie 16')
 
 all_items = [1,2,3,4,5]
@@ -68,6 +75,7 @@ condition = lambda x: x > 3
 not_all_items = not all(condition(x) for x in all_items)
 some_not_all = any(not condition(x) for x in all_items)
 
+print(not_all_items == some_not_all)
 
 """Zadanie 21"""
 
