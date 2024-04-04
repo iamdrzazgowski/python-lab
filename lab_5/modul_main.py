893eab89adda0c8d035eea53bcf352061670aa81
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-04-04 11:08:01 +0200

    lab_5

--- lab_5/modul_main.py
+++ lab_5/modul_main.py
@@ -1,6 +1,13 @@
 import modul as md
 # from modul import f1
 # from modul import *
+from math import *
+from random import *
 
 print(md.f2(10))
 # print(f1(25))
+
+print(cos(pi / 4))
+print(random())
+print(randint(1, 100))
+print(choices("ACTG", weights=(1, 1, 1, 2)))
