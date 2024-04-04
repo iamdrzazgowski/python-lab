39b23ecb7ac8c722e4db2c06fde3fa0e00a311f8
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-04-04 10:11:19 +0200

    lab_5

--- lab_5/cwiczenia.py
+++ lab_5/cwiczenia.py
@@ -2,3 +2,6 @@
 
 dane = (2, 'samochody', 2376.85, 2376.85)
 print(dane[0], dane[1], "o masie", dane[2], "kilograma..")
+
+s = "%d %s o masie %f kilograma \n(w notacji wykładniczej %e kilograma)"
+print(s % dane)
