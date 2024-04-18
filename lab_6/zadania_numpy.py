8ea6ac996d13d1153afbc4344451882b25fed703
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-04-18 12:01:10 +0200

    zadania z biblioteki numpy

--- lab_6/zadania_numpy.py
+++ lab_6/zadania_numpy.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 t = np.array([1, 2, 3])  # tworzenie tablicy
 t1 = np.zeros((3, 5))  # tablica wypełniona zerami
@@ -14,7 +15,18 @@ x2 = np.array([i for i in range(11)])
 
 x3 = np.array(([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
-x = [i for i in range(1, 11)]
-y = list(map(lambda x: x ** 2, x))
-plt.plot(x, y, marker="o", linestyle="dashed")
+# x = [i for i in range(1, 11)]
+# y = list(map(lambda x: x ** 2, x))
+# plt.scatter(x, y, marker="o")
+# plt.show()
+
+# x = [i/100 for i in range(0,628)]
+# y = list(map(lambda i: math.sin(i), x))
+# plt.scatter(x,y)
+# plt.show()
+
+x = np.linspace(-1, 1, 20)
+plt.scatter(x, x**2)
+plt.scatter(x, x**3)
+# plt.legend()
 plt.show()
