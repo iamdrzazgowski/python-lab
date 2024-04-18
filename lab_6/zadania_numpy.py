0ae9f8be27a9e4ea1e3ef16e12b327acf8028b67
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-04-18 11:31:07 +0200

    zadania z biblioteki numpy

---
+++ lab_6/zadania_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+t = np.array([1,2,3])                       #tworzenie tablicy
+t1 = np.zeros((3,5))                        #tablica wypełniona zerami
+t2 = np.ones(10)                            #tablica wypełniona jedynkami
+
+l = np.linspace(0,1,11)     #tworzenie przestrzeni liniowej
+l1 = np.linspace(0,1,6)
+l2 = np.arange(0,1,0.2)
+print(l1)
+print(l2)
