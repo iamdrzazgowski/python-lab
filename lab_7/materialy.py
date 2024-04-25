248e0efa54b6c4c96dcd137a5c21c75e6a99869f
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-04-25 11:00:33 +0200

    lab_7 - zadania

--- lab_7/materialy.py
+++ lab_7/materialy.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 1, 51)
-plt.figure(figsize=(12,10))
-for a in np.linspace(0.25, 2, 8):
-    plt.plot(x, x**a, color=(a/2, 0, 1-a/2), label='$x^{%s}$' % a)
-plt.legend()
+# x = np.linspace(0, 1, 51)
+# plt.figure(figsize=(12,10))
+# for a in np.linspace(0.25, 2, 8):
+#     plt.plot(x, x**a, color=(a/2, 0, 1-a/2), label='$x^{%s}$' % a)
+# plt.legend()
+# plt.show()
+
+
+# T = np.linspace(0, 2*np.pi, 1000)
+# plt.scatter(np.sin(T), np.cos(T))
+# plt.show()
+
+X = np.linspace(-3,3,500)
+Y = 1/(X**2-1)
+Y[abs(Y) > 5] = np.NaN
+plt.figure(figsize=(15,5))
+plt.plot(X,Y)
+plt.plot([-1, -1], [-10,10], '--')
 plt.show()
